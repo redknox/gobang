d86decd1e6938c6b6e1a5444610b10ea9f881674
redknox <redknox@gmail.com>
2019-12-20 11:11:50 +0800

    重新调整了结构。1、创建了用玩家类。可以任意设置真人玩家和AI玩家.2、梳理流程。3、用棋子图片代替了程序绘制棋子。4、用标志的方式同步主程序和选手走棋。

--- gobang.py
+++ gobang.py
@@ -6,42 +6,81 @@
 import pygame
 import sys
 from pygame.locals import *
+import abc
 
 ########################################################################################################################
 FPS = 50  # 帧数
 WIDE = 771  # 工作区为长宽为751的正方形
-LINES = 15  # 标准围棋盘，有十九条线
+LINES = 15  # 标准五子棋棋盘，有十五条线
 cellWidth = (WIDE - 20) // LINES  # 格子宽度
-W = WIDE // cellWidth  #
-# 定义颜色
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-YELLOW = (255, 255, 0)
+current_po = (-1, -1)  # 最后一个落子位置，（-1，-1）表示当前数据不可用
+flag = 0  # 同步标志，0为初始化，1为已落子，2为等待人工落子，3为等待AI落子
 
-if __name__ == '__main__':
-    print('main')
+
+#####################
+# 设置标志,因为对象无法直接操作全局变量，特别用这个函数来操作
+# 输入：int id 状态类型：0 初始化 1 已经落子，等待判定 2 等待人工落子 3 等待AI落子
+#####################
+
+def set_flag(id):
+    global flag
+    flag = id
+
+
+#####################
+#
+# 棋手类.所有棋手的基类
+#
+#####################
+
+class Player(metaclass=abc.ABCMeta):
+    id = 0  # 棋手ID，黑棋为1，白棋为2
+    name = ''  # 棋手姓名
+    type = 0  # 玩家类型，2 为真人玩家，3为ai玩家 这个类型等同于状态，表示等待真人玩家落子和等待AI玩家落子
+
+    # 落子程序,抽象方法，必须在子类中实现
+    @abc.abstractmethod
+    def move(self):
+        pass
+
+
+#####################
+#
+# 真人棋手类
+#
+#####################
+
+class Human_player(Player):
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+        self.type = 2  # 指定当前玩家为真人玩家
+
+    def move(self):
+        set_flag(2)  # 只需要修改标志位为2，等待系统接收键盘点击事件即可
 
 
 #####################
 # 绘制棋子
 # input: x,y 落子在棋盘上的位置，color 落子颜色
+# output: 无
 #####################
-def draw_piece(x, y, color):
+def draw_piece(po, id):
     # 不进行合法性检查了
-    x = x * cellWidth + cellWidth // 2 + 10
-    y = y * cellWidth + cellWidth // 2 + 10
-    pygame.draw.circle(windowSurface, color, (x, y), cellWidth // 2 - 2, 0)
-    pygame.draw.circle(windowSurface, BLACK, (x, y), cellWidth // 2 - 2, 1)
+    x = po[0] * cellWidth + 10
+    y = po[1] * cellWidth + 10
+    if (id == 1):
+        windowSurface.blit(go_piece_black, (x, y))
+    else:
+        windowSurface.blit(go_piece_white, (x, y))
 
 
 #####################
-# 判断是否获胜
-# input x,y 落子位置,player 玩家编号
+# 获胜判定
+# input x,y 落子位置,player 玩家编号,可选值是1或2
 # output ture/false 是否获胜
 #####################
-def judge_victory(unit_x, unit_y, player):
-    max = 0  # 各个方向上连成一线棋子的最大值
-
+def judge_victory(po, player):
     # 分别计算横、竖线、左斜、右斜四条直线上连成一线棋子的总数，如果有5个则比赛获胜
     # 先计算从落子出发一个方向的格子是否和落子有一样颜色的棋子，是的话count+1，继续判断该方向下一个格子，直到碰到第一个没有棋子或者有棋子颜色不同的格子
     # 然后计算落子另一个方向的棋子，算法同前
@@ -51,86 +90,115 @@ def judge_victory(unit_x, unit_y, player):
         for direction in (1, -1):  # 两个方向
             x, y = x * direction, y * direction
 
-            current_x, current_y = unit_x + x, unit_y + y  # 计算在当前直线上，沿着当前当前方向下一个格子的位置
+            current_x, current_y = po[0] + x, po[1] + y  # 计算在当前直线上，沿着当前当前方向下一个格子的位置
             if current_x < 0 or current_y < 0 or current_x >= LINES or current_y >= LINES:  # 如果超出棋盘则退出当前循环，换下一方向或者换下一条直线
                 break
 
             while a[current_x][current_y] == player:
                 count += 1
                 if count == 5:
-                    print('player', player, 'win!')
-                    return True
+                    return True  # 返回获胜
                 current_x += x
                 current_y += y
                 if current_x < 0 or current_y < 0 or current_x >= LINES or current_y >= LINES:
                     break
 
-        if count > max:  # 保存最大的单行连续棋子数
-            max = count
-
-    print(max)
-    return False
+    return False  # 返回未获胜
 
 
-a = []  # 构造数组a放置每个格子的信息
-for i in range(LINES):
-    a.append([])
-    for j in range(LINES):
-        a[i].append(0)  # 棋盘上每个格子初始为空
+#####################
+# 点击落子
+#####################
+def u_click(mouse_x, mouse_y):
+    if mouse_x < 25 or mouse_y < 20 or mouse_x > 761 or mouse_y > 761:
+        return (-1, -1)
 
-pygame.init()
-clock = pygame.time.Clock()
-windowSurface = pygame.display.set_mode((WIDE, WIDE), 0, 32)
-pygame.display.set_caption('孔海峰的五子棋练习')
+    # 根据点击位置计算落子位置
+    x = (mouse_x - 10) // cellWidth
+    y = (mouse_y - 10) // cellWidth
 
-# 绘制背景
-background = pygame.image.load("gobang_plate.jpeg")
-background = pygame.transform.scale(background, (WIDE, WIDE))
-windowSurface.blit(background, (0, 0))
-pygame.display.update()
+    # 判断当前位置上是否有棋子，有的话本次点击无效
+    if a[x][y] != 0:
+        return (-1, -1)
 
-# 初始化
-# 注意，当循环开始后，会先更换当前玩家，所以将当前玩家设置为白棋玩家
-currentColor = WHITE
-player = 2
-debug = []
+    return (x, y)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        # 等待玩家落子
-        if event.type == MOUSEBUTTONDOWN:
-            mouse_x, mouse_y = event.pos
-            # 合法性检查
-            if mouse_x < 25 or mouse_y < 20 or mouse_x > 761 or mouse_y > 761:
-                break
-            # 根据点击位置计算落子位置
-            unit_x = (mouse_x - 10) // cellWidth
-            unit_y = (mouse_y - 10) // cellWidth
 
-            # 判断当前位置上是否有棋子，有的话本次点击无效
-            if a[unit_x][unit_y] != 0:
-                break
+if __name__ == '__main__':
+    pygame.init()
+    windowSurface = pygame.display.set_mode((WIDE, WIDE), 0, 32)
+    pygame.display.set_caption('孔海峰的五子棋练习')
 
-            # 转换玩家
-            # 可以在这里写
-            if player == 2:
-                currentColor = BLACK
-                player = 1
-            else:
-                currentColor = WHITE
-                player = 2
+    # 读取棋盘、棋子的资源文件
+    go_piece_black = pygame.image.load("go_piece_black.png").convert_alpha()
+    go_piece_white = pygame.image.load("go_piece_white.png").convert_alpha()
+    background = pygame.image.load("gobang_plate.jpeg")  # 读取背景图片
+    background = pygame.transform.scale(background, (WIDE, WIDE))  # 调整背景图片大小
+    windowSurface.blit(background, (0, 0))  # 绘制背景
+    # 读取黑白棋图片
+    go_piece_black = pygame.transform.scale(go_piece_black, (50, 50))
+    go_piece_white = pygame.transform.scale(go_piece_white, (50, 50))
+    # 初始化棋盘
+    pygame.display.update()
 
-            # 落子
-            draw_piece(unit_x, unit_y, currentColor)
+    # 初始化棋盘数组
+    a = []
+    for i in range(LINES):
+        a.append([])
+        for j in range(LINES):
+            a[i].append(0)  # 棋盘上每个格子初始为空
+
+    # 初始化时钟
+    clock = pygame.time.Clock()
+
+    # 初始化玩家
+
+    player1 = Human_player(1, 'kong')
+
+    player2 = Human_player(2, 'Haifeng')
+
+    currentPlayer = player1
+
+    # flag 是标志，0表示初始化，1表示当前玩家已经落子，2表示等待人工落子，3表示等待AI落子
+    flag = currentPlayer.type
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == MOUSEBUTTONDOWN:
+                # 如果当前状态为等待玩家落子
+                if flag == 2:
+                    mouse_x, mouse_y = event.pos
+                    current_po = u_click(mouse_x, mouse_y)
+
+                    if current_po == (-1, -1):
+                        break
+                    else:
+                        flag = 1  # 将状态设置为已经落子
+
+        # 如果已经落子
+        if flag == 1:
+            # 重绘棋盘
+            draw_piece(current_po, currentPlayer.id)
             pygame.display.update()
 
-            a[unit_x][unit_y] = player  # 将当前玩家落子存入数组
+            a[current_po[0]][current_po[1]] = currentPlayer.id  # 将当前玩家落子存入数组
 
-            if judge_victory(unit_x, unit_y, player):
-                print("我赢了，哈哈哈哈哈哈啊哈哈！")
+            # 获胜判定
+            if judge_victory(current_po, currentPlayer.id):
+                print(currentPlayer.name + "我赢了，哈哈哈哈哈哈啊哈哈！")
 
+            # TODO：记录棋型
 
-    time_passed = clock.tick(FPS)
+            # 转换玩家
+            if currentPlayer == player1:
+                currentPlayer = player2
+            else:
+                currentPlayer = player1
+
+            # 当前玩家走子
+            currentPlayer.move()
+
+        time_passed = clock.tick(FPS)
